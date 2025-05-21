25f9d6ee975c977b6750a56d41a5f4c76ea90366
doerrzoran <doerrzoran@gmail.com>
2025-05-21 15:11:26 +0200

    update

--- src/Controller/CollaboratorController.php
+++ src/Controller/CollaboratorController.php
@@ -149,15 +149,14 @@ final class CollaboratorController extends AbstractController
         }
     }
 
-    #[Route('/collaborator/update', name: 'app_collaborator_update', methods: ['PUT'])]
-    public function updateCollaborator(Request $request): JsonResponse
+    #[Route('/collaborator/update/{id}', name: 'app_collaborator_update', methods: ['PUT'])]
+    public function updateCollaborator(Request $request, int $id): JsonResponse
     {
         $data = json_decode($request->getContent(), true);
         if (!$data) {
             return $this->json(['error' => 'Données manquantes pour la mise à jour.'], 400);
         }
         try {
-            $id = $request->request->get('id');
             $collaborator = $this->collaboratorService->getById($id);
             // Mise à jour des propriétés (à adapter selon tes besoins)
             if (isset($data['firstname'])) $collaborator->setFirstname($data['firstname']);
